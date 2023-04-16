f03ee90de0fe9f28a0d799a0a9faed968296db3f
morisgateno-appsflyer <moris.gateno@appsflyer.com>
2023-04-16 08:35:53 +0300

    first-commit

---
+++ IDFVWhiteLabelTestingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDFVWhiteLabelTestingApp
+//
+//  Created by Moris Gateno on 13/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var IDFVLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        displayIDFV()
+    }
+    func displayIDFV(){
+        self.IDFVLabel.text = UIDevice.current.identifierForVendor?.uuidString ?? "No IDFV"
+        self.IDFVLabel.sizeToFit()
+    }
+
+}
+
